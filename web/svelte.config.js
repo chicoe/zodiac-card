e617849f508d9d7421962eaecd35e21b5dae76e2
Francisco Estivallet <23107550+chicoe@users.noreply.github.com>
2026-04-01 02:11:37 +0200

    change path for ghpages deploy

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -11,7 +11,7 @@ const config = {
 			fallback: 'index.html'
 		}),
 		paths: {
-			base: dev ? '' : process.env.BASE_PATH || '/zodiac-card'
+			base: dev ? '' : process.env.BASE_PATH || ''
 		}
 	}
 };
